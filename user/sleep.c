6f92fcd66dad0f1493819deec41504b2c734021e
brant-skywalker <1773159722@qq.com>
2022-06-03 20:50:03 +0800

    `sleep` completed.

---
+++ user/sleep.c
@@ -0,0 +1,15 @@
+#include "../kernel/types.h"
+#include "../kernel/stat.h"
+#include "../user/user.h"
+
+int main(int argc, char* argv[]) {
+    int i;
+
+    if (argc <= 1) {
+        fprintf(2, "usage: sleep [# ticks...]")
+        exit(1);
+    }
+
+    sleep(atoi(argv[1]));
+    exit(0);
+}
