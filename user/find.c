77866397f375bd5c695a69ba12884629374bd81e
brant-skywalker <1773159722@qq.com>
2022-06-05 21:46:33 +0800

    `find` still testing...

--- user/find.c
+++ user/find.c
@@ -1,70 +1,138 @@
-#include "../kernel/types.h"
-#include "../kernel/stat.h"
-#include "../user/user.h"
-#include "../kernel/fs.h"
+//#include "../kernel/types.h"
+//#include "../kernel/stat.h"
+//#include "../user/user.h"
+//#include "../kernel/fs.h"
+//
+//void find(char* path, char* pattern) {
+//    char buf[512];
+//    char* p;
+//    int fd;
+//    struct dirent de;
+//    struct stat st;
+//
+//    if (0 > (fd = open(path, 0))) {
+//        fprintf(2, "find: cannot open %s\n", path);
+//        return;
+//    }
+//
+//    if (0 > fstat(fd, &st)) {
+//        fprintf(2, "find: cannot stat %s\n", path);
+//        close(fd);
+//        return;
+//    }
+//
+//    switch (st.type) {
+//        case T_FILE:
+//            fprintf(2, "usage: find [starting directory] [pattern]\n");
+//            break;
+//        case T_DIR:
+//            if (sizeof(buf) < strlen(path) + 1 + DIRSIZ + 1) {
+//                printf("find: path too long\n");
+//                break;
+//            }
+//            strcpy(buf, path);  // Save the path.
+//            p = buf + strlen(buf);   // Move `p` to the end of path.
+//            *p++ = '/';  // Attach a slash to it.
+//            while (sizeof(de) == read(fd, &de, sizeof(de))) {  // Dirent read successfully.
+//                if (0 == de.inum || 0 == strcmp(de.name, ".") || 0 == strcmp(de.name, "..")) {
+//                    continue;
+//                }
+//                memmove(p, de.name, DIRSIZ);
+//                p[DIRSIZ] = 0;
+//                if (0 > stat(buf, &st)) {
+//                    fprintf(2, "find: cannot stat %s\n", buf);
+//                    continue;
+//                }
+//                switch (st.type) {
+//                    case T_FILE:
+//                        if (0 == strcmp(de.name, pattern)) {
+//                            printf("%s\n", buf);
+//                        }
+//                        break;
+//                    case T_DIR:
+//                        find(buf, pattern);
+//                        break;
+//                }
+//                break;
+//            }
+//    }
+//    close(fd);
+//}
+//
+//int main(int argc, char* argv[]) {
+//    if (3 != argc) {
+//        fprintf(2, "usage: find [starting directory] [pattern]\n");
+//        exit(1);
+//    }
+//    find(argv[1], argv[2]);
+//    exit(0);
+//}
 
-void find(char* path, char* pattern) {
-    char buf[512];
-    char* p;
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+#include "kernel/fs.h"
+
+void find_helper(char const *path, char const *target)
+{
+    char buf[512], *p;
     int fd;
     struct dirent de;
     struct stat st;
-
-    if (0 > (fd = open(path, 0))) {
+    if((fd = open(path, 0)) < 0){
         fprintf(2, "find: cannot open %s\n", path);
-        return;
+        exit(1);
     }
 
-    if (0 > fstat(fd, &st)) {
+    if(fstat(fd, &st) < 0){
         fprintf(2, "find: cannot stat %s\n", path);
-        close(fd);
-        return;
+        exit(1);
     }
 
-    switch (st.type) {
+    switch(st.type){
         case T_FILE:
-            fprintf(2, "usage: find [starting directory] [pattern]\n");
-            break;
+            fprintf(2, "Usage: find dir file\n");
+            exit(1);
         case T_DIR:
-            if (sizeof(buf) < strlen(path) + 1 + DIRSIZ + 1) {
+            if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
                 printf("find: path too long\n");
                 break;
             }
-            strcpy(buf, path);  // Save the path.
-            p = buf + strlen(buf);   // Move `p` to the end of path.
-            *p++ = '/';  // Attach a slash to it.
-            while (sizeof(de) == read(fd, &de, sizeof(de))) {  // Dirent read successfully.
-                if (0 == de.inum || 0 == strcmp(de.name, ".") || 0 == strcmp(de.name, "..")) {
+            strcpy(buf, path);
+            p = buf + strlen(buf);
+            *p++ = '/';
+            while(read(fd, &de, sizeof(de)) == sizeof(de)){
+                if (de.inum == 0 || strcmp(de.name, ".") == 0 || strcmp(de.name, "..") == 0)
                     continue;
-                }
                 memmove(p, de.name, DIRSIZ);
                 p[DIRSIZ] = 0;
-                if (0 > stat(buf, &st)) {
-                    fprintf(2, "find: cannot stat %s\n", buf);
+                if(stat(buf, &st) < 0){
+                    printf("find: cannot stat %s\n", buf);
                     continue;
                 }
-                printf("%s %d %d %d\n", buf, st.type, st.ino, st.size);
-                switch (st.type) {
-                    case T_FILE:
-                        if (0 == strcmp(de.name, pattern)) {
-                            printf("%s\n", buf);
-                        }
-                        break;
-                    case T_DIR:
-                        find(buf, pattern);
-                        break;
+                if(st.type == T_DIR){
+                    find_helper(buf, target);
+                }else if (st.type == T_FILE){
+                    if (strcmp(de.name, target) == 0)
+                    {
+                        printf("%s\n", buf);
+                    }
                 }
-                break;
             }
+            break;
     }
     close(fd);
 }
-
-int main(int argc, char* argv[]) {
-    if (3 != argc) {
-        fprintf(2, "usage: find [starting directory] [pattern]\n");
+int main(int argc, char const *argv[])
+{
+    if (argc != 3)
+    {
+        fprintf(2, "Usage: find dir file\n");
         exit(1);
     }
-    find(argv[1], argv[2]);
+
+    char const *path = argv[1];
+    char const *target = argv[2];
+    find_helper(path, target);
     exit(0);
 }
