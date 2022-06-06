a774d87ba221202535e580677e8c2c02a70af431
brant-skywalker <1773159722@qq.com>
2022-06-06 21:14:33 +0800

    `xargs` testing.

--- user/xargs.c
+++ user/xargs.c
@@ -57,7 +57,7 @@ int main(int argc, char* argv[]) {
         printf("%s\n", args[i - 2]);
     }
     while (get_argv(args, argc - 2)) {
-        printf("Command: %s, Arguments: %s\n", cmd, args);
+        printf("Command: %s, Arguments: %s, %s, %s\n", cmd, args[0], args[1], args[2]);
         if (0 == fork()) {
             exec(cmd, args);
             fprintf(2, "exec %s failed\n", cmd);
