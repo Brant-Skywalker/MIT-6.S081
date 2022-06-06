221642263feace4ed5e7063c5783b8572098e935
brant-skywalker <1773159722@qq.com>
2022-06-06 21:11:39 +0800

    `xargs` testing.

--- user/xargs.c
+++ user/xargs.c
@@ -16,6 +16,7 @@ int get_argv(char** args, int i) {
     int n = 0;
     char buf[BUFSIZE];
     while (1 != read(0, buf + n, 1)) {  // Read until newline character met.
+        printf("%c\n", buf[n]);
         if ('\n' == buf[n++]) { break; }
         if (BUFSIZE == n) {
             fprintf(2, "xargs: input arg too long\n");
