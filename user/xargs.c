d1774e6da01f93b67544fb94f140ac7c827e1f99
brant-skywalker <1773159722@qq.com>
2022-06-06 20:46:16 +0800

    `xargs` testing.

--- user/xargs.c
+++ user/xargs.c
@@ -2,8 +2,65 @@
 #include "../kernel/stat.h"
 #include "../user/user.h"
 #include "../kernel/fs.h"
+#include "../kernel/param.h"
+
+/*!
+ * @brief This function reads an individual line of input argument.
+ * NOTE: This function reads arguments from file descriptor 0.
+ * @param args is array of character strings (resulting argv).
+ * @param i is the original number of arguments.
+ * @return 1 if new argv added, 0 otherwise.
+ */
+int get_argv(char** args, int i) {
+    const int BUFSIZE = 512;
+    int n = 0;
+    char buf[BUFSIZE];
+    while (1 != read(0, buf, 1)) {  // Read until newline character met.
+        if ('\n' == buf[n++]) { break; }
+        if (BUFSIZE == n) {
+            fprintf(2, "xargs: input arg too long\n");
+            exit(1);
+        }
+    }
+    if (0 == n) { return 0; }  // No argument read from fd 0.
+    buf[n] = '\0';  // Terminate with nul.
+    int p = 0;
+    while (p < n) {   // Now split the line by spaces.
+        while (p < n && ' ' != buf[p]) { ++p; }
+        if (p < n) { buf[p] = '\0'; }  // Split the string.
+        args[i] = malloc(strlen(buf) + 1);
+        strcpy(args[i++], buf);  // Copy the new argument and increment argc.
+        while (p < n && ' ' == buf[p]) { ++p; }
+        if (p < n) { buf += p; }
+    }
+    args[i] = 0;  // End of argv.
+    return 1;
+}
 
 int main(int argc, char* argv[]) {
-
-    return 0;
+    if (2 > argc) {
+        fprintf(2, "usage: xargs [command [initial-arguments]]\n");
+        exit(1);
+    }
+    char cmd[strlen(argv[1]) + 1];  // The passed-in command.
+    strcpy(cmd, argv[1]);
+    char* args[MAXARG];  // Array of pointers.
+    for (int i = 1; i < argc; ++i) {  // Save initial arguments.
+        args[i - 1] = malloc(strlen(argv[i]) + 1);
+        strcpy(args[i - 1], argv[i]);
+    }
+    while (get_argv(args, argc - 1)) {
+        if (0 == fork()) {
+            exec(cmd, args);
+            fprintf(2, "exec %s failed\n", cmd);
+            exit(1);
+        } else {
+            wait((int*) 0);
+        }
+    }
+    for (int i = 0; i < MAXARG; ++i) {
+        if (0 == args[i]) { break; }
+        free(args[i]);
+    }
+    exit(0);
 }
