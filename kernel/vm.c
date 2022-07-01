ac8a1ffbc45b6e5c578f5e74d9daebb2cce7b2c0
brant-skywalker <1773159722@qq.com>
2022-07-01 18:12:10 +0800

    Testing lazy: disabled uvmunmap panic: walk

--- kernel/vm.c
+++ kernel/vm.c
@@ -180,10 +180,10 @@ uvmunmap(pagetable_t pagetable, uint64 va, uint64 npages, int do_free)
     panic("uvmunmap: not aligned");
 
   for(a = va; a < va + npages*PGSIZE; a += PGSIZE){
-    if((pte = walk(pagetable, a, 0)) == 0)
-      panic("uvmunmap: walk");
-    if((*pte & PTE_V) == 0)
+    // It's now okay if a pte does not exist in pagetable or has invalid bit set.
+    if (0 == (pte = walk(pagetable, a, 0)) || 0 == (*pte & PTE_V)) {
       continue;
+    }
     if(PTE_FLAGS(*pte) == PTE_V)
       panic("uvmunmap: not a leaf");
     if(do_free){
