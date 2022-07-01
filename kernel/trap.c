ae78d6a334110657cd7e4a46d9a98b3ce622eaa8
brant-skywalker <1773159722@qq.com>
2022-07-01 17:48:31 +0800

    Testing lazy

--- kernel/trap.c
+++ kernel/trap.c
@@ -69,7 +69,7 @@ usertrap(void)
     // ok
   } else if (13 == r_scause() || 15 == r_scause()) {
     uint64 va = PGROUNDDOWN(r_stval());
-    printf("page fault %p\n", va);
+    /* printf("page fault %p\n", va); */
     uint64 ka = (uint64) kalloc();
     if (0 == ka) {
       p->killed = 1;
