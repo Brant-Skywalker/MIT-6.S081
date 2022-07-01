1005863f0a3c65726b49af673478a9dbeff8da86
brant-skywalker <1773159722@qq.com>
2022-07-01 17:45:07 +0800

    Testing lazy

--- kernel/trap.c
+++ kernel/trap.c
@@ -68,18 +68,17 @@ usertrap(void)
   } else if((which_dev = devintr()) != 0){
     // ok
   } else if (13 == r_scause() || 15 == r_scause()) {
-    char* mem;
     uint64 va = PGROUNDDOWN(r_stval());
-
-    if (0 == (mem = kalloc())) {
-      exit(-1);
-    }
-
-    memset(mem, 0, PGSIZE);
-
-    if (0 != mappages(p->pagetable, va, PGSIZE, (uint64) mem, PTE_R | PTE_W | PTE_X | PTE_U)) {
-      kfree(mem);
-      exit(-1);
+    uint64 ka = (uint64) kalloc();
+    
+    if (0 == ka) {
+      p->killed = 1;
+    } else {
+      memset((void*) ka, 0, PGSIZE);
+      if (0 != mappages(p->pagetable, va, PGSIZE, ka, PTE_R | PTE_W | PTE_U)) {
+        kfree((void*) ka);
+        p->killed = 1;
+      }
     }
   } else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
